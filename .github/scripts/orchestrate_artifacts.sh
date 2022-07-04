0a7aa3e4afd43bc3fb4e59f3f260892dd365868a
rrigato <ryanrigato@gmail.com>
2022-07-04 10:34:49 -0500

    add the content-type for the s3 object image

--- .github/scripts/orchestrate_artifacts.sh
+++ .github/scripts/orchestrate_artifacts.sh
@@ -5,4 +5,5 @@ set -e
 
 aws s3api put-object --bucket $AWS_S3_BUCKET_NAME \
   --key $AWS_S3_UPLOAD_PREFIX \
-  --body "/metrics_renders/${COMMIT_CALENDAR_FILE}"
+  --body "/metrics_renders/${COMMIT_CALENDAR_FILE}" \
+  --content-type "image/svg+xml"
