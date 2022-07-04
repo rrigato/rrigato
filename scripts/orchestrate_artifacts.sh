44874718a3a66073e352774be70a64a44acf6536
rrigato <ryanrigato@gmail.com>
2022-07-04 09:29:28 -0500

    extracted orchestration script

---
+++ scripts/orchestrate_artifacts.sh
@@ -0,0 +1,12 @@
+aws --version
+env
+aws sts get-caller-identity
+#   aws s3api put-object --bucket $AWS_S3_BUCKET_NAME \
+#   --key "${AWS_S3_KEY_PREFIX}/" --body my_images.tar.bz2
+
+ls -l
+
+cd /metrics_renders
+
+ls -l
+      
