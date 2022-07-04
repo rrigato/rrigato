75f50fc4e9d83d1320d3327b78c08675112aff9a
rrigato <ryanrigato@gmail.com>
2022-07-04 09:15:15 -0500

    deployment script and remove commit calendar from repo

---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+#exits program immediately if a command is not sucessful
+set -e
+
+source avenv/bin/activate
+
+secret_scan_results=$(detect-secrets scan | \
+python3 -c "import sys, json; print(json.load(sys.stdin)['results'])" )
+
+# static scan for security credentials that terminates if any secrets are found
+if [ "${secret_scan_results}" != "{}" ]; then
+    echo "detect-secrets scan failed"
+    exit 125
+fi
+
+git push origin master
--- scripts/scan_repo.sh
+++
@@ -1,2 +0,0 @@
-detect-secrets scan | \
-python3 -c "import sys, json; print(json.load(sys.stdin)['results'])"
