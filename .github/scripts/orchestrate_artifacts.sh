0d4259174fda09665237754c6c3882f3b3d90e7f
rrigato <ryanrigato@gmail.com>
2022-07-04 10:16:50 -0500

    correct location

--- .github/scripts/orchestrate_artifacts.sh
+++ .github/scripts/orchestrate_artifacts.sh
@@ -4,5 +4,5 @@ set -e
 
 
 aws s3api put-object --bucket $AWS_S3_BUCKET_NAME \
-  --key "${AWS_S3_KEY_PREFIX}/${COMMIT_CALENDAR_FILE}" \
+  --key "${AWS_S3_KEY_PREFIX}${COMMIT_CALENDAR_FILE}" \
   --body "/metrics_renders/${COMMIT_CALENDAR_FILE}"
