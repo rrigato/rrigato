97b8e21ad4bef5470bc7f16285d605c54e5b0556
rrigato <ryanrigato@gmail.com>
2022-07-10 11:53:16 -0500

    streamline update script

--- .github/scripts/orchestrate_artifacts.sh
+++ .github/scripts/orchestrate_artifacts.sh
@@ -4,6 +4,11 @@ set -e
 
 
 aws s3api put-object --bucket $AWS_S3_BUCKET_NAME \
-  --key $AWS_S3_UPLOAD_PREFIX \
+  --key "${AWS_S3_UPLOAD_PREFIX}${COMMIT_CALENDAR_FILE}" \
   --body "/metrics_renders/${COMMIT_CALENDAR_FILE}" \
+  --content-type "image/svg+xml"
+
+aws s3api put-object --bucket $AWS_S3_BUCKET_NAME \
+  --key "${AWS_S3_UPLOAD_PREFIX}${LANGUAGES_FILE}" \
+  --body "/metrics_renders/${LANGUAGES_FILE}" \
   --content-type "image/svg+xml"
