7dca8ea824d4355c052e8ceaf4aa5d1cc90c0354
rrigato <ryanrigato@gmail.com>
2022-07-16 12:07:46 -0500

    Add cache control header

--- .github/scripts/orchestrate_artifacts.sh
+++ .github/scripts/orchestrate_artifacts.sh
@@ -6,9 +6,11 @@ set -e
 aws s3api put-object --bucket $AWS_S3_BUCKET_NAME \
   --key "${AWS_S3_UPLOAD_PREFIX}${COMMIT_CALENDAR_FILE}" \
   --body "/metrics_renders/${COMMIT_CALENDAR_FILE}" \
-  --content-type "image/svg+xml"
+  --content-type "image/svg+xml" \
+  --cache-control "max-age 86400"
 
 aws s3api put-object --bucket $AWS_S3_BUCKET_NAME \
   --key "${AWS_S3_UPLOAD_PREFIX}${LANGUAGES_FILE}" \
   --body "/metrics_renders/${LANGUAGES_FILE}" \
-  --content-type "image/svg+xml"
+  --content-type "image/svg+xml" \
+  --cache-control "max-age 86400"
