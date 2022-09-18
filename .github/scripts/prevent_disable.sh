7c58658997dde4c237f81c7125ca73de0c3fd2ae
rrigato <ryanrigato@gmail.com>
2022-09-18 11:16:16 -0700

    complicated tag processing logic

--- .github/scripts/prevent_disable.sh
+++ .github/scripts/prevent_disable.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-current_tags=$(git tag | tail -1 )
+current_tags=$(git tag | tail -1 | sed 's/\./replace/2')
 
 git tag 
 
 git status
 
 echo "---current status---"
-echo $current_tags
+echo ${current_tags#remove}
 
 if [ $(date +%d) = "18" ]; then
     echo "adding unnecessary tag"
